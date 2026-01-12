e8508d5ce9dab85d8bdd3ae6fc39d6b330123e60
yyyuk16 <2022rd106@gmail.com>
2026-01-12 14:08:15 +0900

    Reset GitHub to only include templates folder

--- functions/sindex.js
+++
@@ -1,5 +0,0 @@
-const functions = require('firebase-functions');
-const { chatWithGemini } = require('./geminiChat');  // geminiChat.jsのインポート
-
-// エクスポートして、Firebase Functionsとして利用できるようにする
-exports.chatWithGemini = functions.https.onRequest(chatWithGemini);
